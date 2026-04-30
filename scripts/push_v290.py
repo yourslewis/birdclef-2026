deae42c33a4ed7bf59ff8c5124b12e0bb83438cc
yourslewis <5415708+yourslewis@users.noreply.github.com>
2026-04-29 17:42:48 -0700

    Add v290 probability smoothing gamma high experiment

---
+++ scripts/push_v290.py
@@ -0,0 +1,9 @@
+"""Push v290 Kaggle kernel with KGAT Bearer auth via Kaggle API 2.x."""
+import json, os
+from kaggle.api.kaggle_api_extended import KaggleApi
+with open(os.path.expanduser('~/.kaggle/kaggle.json')) as f:
+    os.environ['KAGGLE_API_TOKEN'] = json.load(f)['key']
+api=KaggleApi(); api.authenticate()
+folder=os.path.expanduser('~/Documents/birdclef-2026/kaggle-kernels/v290-prob-smooth-gamma090')
+print('Pushing kernel v290-prob-smooth-gamma090...')
+print('Kernel push result:', api.kernels_push(folder))
